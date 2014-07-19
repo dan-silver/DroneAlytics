20b726e695f09a6be964d6f5340c6e755c2a8f11
JoeLoser <joeloser93@gmail.com>
2014-07-19 14:52:02 -0500

    Fixed host options path with API.

--- app.js
+++ app.js
@@ -26,18 +26,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 }
 
-app.get('/', function(req, res){
-  res.render('index', { title: 'Express' });
+app.get('/', function(req, res) {
+  res.render('index', {
+      title: 'Express'
+  });
   face.face();
 });
 
-// Face++ API
-var API_KEY = 9d6a99bbb5fe5a0b10d59dc10dbf7b16;
-var API_SECRET = xuv78oYAF - TgYy - tEtjWXIiKtFwlQIrc;
-
 http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
--- face++.js
+++ face++.js
@@ -1,30 +1,33 @@
 var http = require('http');
 
-module.exports.face = function(){
+module.exports.face = function() {
+    // Face++ API
+    var API_KEY = 9d6a99bbb5fe5a0b10d59dc10dbf7b16;
+    var API_SECRET = xuv78oYAF-TgYy-tEtjWXIiKtFwlQIrc;
 
-var picURL = "http%3A%2F%2Ffaceplusplus.com%2Fstatic%2Fimg%2Fdemo%2F1.jpg"
-var api_secret
-var attribute = "glass,pose,gender,age,race,smiling"
+    var picURL = "http%3A%2F%2Ffaceplusplus.com%2Fstatic%2Fimg%2Fdemo%2F1.jpg"
+    var attribute = "glass,pose,gender,age,race,smiling"
 
-//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: 'https://apius.faceplusplus.com',
-  path: '/v2/detection/detect?url='+picURL*'&api_secret=YOUR_API_SECRET&api_key=YOUR_API_KEY&attribute='+attribute
-};
+    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+    var options = {
+        host: 'https://apius.faceplusplus.com',
+        path: '/v2/detection/detect?url=' + picURL + '&api_secret=' + API_SECRET + '&api_key=' +
+            API_KEY + '&attribute=' + attribute
+    };
 
-callback = function(response) {
-  var str = '';
+    callback = function(response) {
+        var str = '';
 
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function(chunk) {
+            str += chunk;
+        });
 
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    console.log(str);
-  });
-}
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function() {
+            console.log(str);
+        });
+    }
 
-http.request(options, callback).end();
+    http.request(options, callback).end();
 }
