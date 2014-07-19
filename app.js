fd1920d96813d9b7109a2cd9ed311c302ef4e428
Sam Kreter <sakfy6@mail.missouri.edu>
2014-07-19 17:38:27 -0500

    added face compare function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var face = require('./face++.js');
-var drone = require('./drone.js');
+//var drone = require('./drone.js');
 
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-drone.startPNGStream()
+//drone.startPNGStream()
 
 // development only
 if ('development' == app.get('env')) {
@@ -33,26 +33,6 @@ if ('development' == app.get('env')) {
 }
 
 
-// Face++ API
-var API_KEY = "9d6a99bbb5fe5a0b10d59dc10dbf7b16";
-var API_SECRET = "xuv78oYAF - TgYy - tEtjWXIiKtFwlQIrc";
-
-
-face.face();
-
-/*
-app.get('/', function(req, res){
-  res.render('index', { title: 'Express' });
-  face.face(API_SECRET,API_KEY);
-=======
-app.get('/', function(req, res) {
-  res.render('index', {
-      title: 'Express'
-  });
-  face.face();
->>>>>>> 20b726e695f09a6be964d6f5340c6e755c2a8f11
-});
-*/
 
 
 http.createServer(app).listen(app.get('port'), function() {
--- face++.js
+++ face++.js
@@ -1,25 +1,21 @@
 var http = require('https');
+var keys = require('./config.js');
 
-module.exports.face = function(api_secret,api_key){
+module.exports.facefind = function(returnit){
 
-//var picURL = "http%3A%2F%2Fwww.faceplusplus.com%2Fwp-content%2Fthemes%2Ffaceplusplus%2Fassets%2Fimg%2Fdemo%2F1.jpg%3Fv%3D2"
-var picURL = "https://scontent-a.xx.fbcdn.net/hphotos-xfa1/t1.0-9/p480x480/10494650_10203694011051443_1904878853767976155_n.jpg"
-var attribute = "glass,pose,gender,age,race,smiling"
-
-var api_key = "5dbb712f5a8a4787da2ab34058b98876"
-var api_secret = "cV-5DUanQmpt2EHIcUozVRfHXVTLlFzN"
+    var picURL = "https://scontent-a.xx.fbcdn.net/hphotos-xfa1/t1.0-9/p480x480/10494650_10203694011051443_1904878853767976155_n.jpg"
+    var attribute = "glass,pose,gender,age,race,smiling"
 
 
-//path: '/v2/detection/detect?url='+picURL*'&api_secret='+api_secret+'&api_key='+api_key+'&attribute='+attribute
-
-//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: 'apius.faceplusplus.com',
-  path: '/v2/detection/detect?api_key='+api_key+'&api_secret='+api_secret+'&url='+picURL+'&attribute=age%2Cgender%2Crace%2Csmiling%2Cpose%2Cglass'
-};
+    var options = {
+      host: 'apius.faceplusplus.com',
+      path: '/v2/detection/detect?api_key='+keys.api_key+'&api_secret='+keys.api_secret+'&url='+picURL+'&attribute=age%2Cgender%2Crace%2Csmiling%2Cpose%2Cglass'
+    };
 
 
-    callback = function(response) {
+
+
+callback = function(response) {
         var str = '';
 
         //another chunk of data has been recieved, so append it to `str`
@@ -29,9 +25,45 @@ var options = {
 
         //the whole response has been recieved, so we just print it out here
         response.on('end', function() {
-            console.log(str);
+            var data = JSON.parse(str);
+            facecompare(data.face[0].face_id,data.face[1].face_id);
         });
-    }
+    }    
 
     http.request(options, callback).end();
+}
+
+
+    facecompare = function(face_id1,face_id2){
+    
+    /*
+    var face_id2 = "2a207e2d79a9746c4a5f6d55fa3e9724"
+    var face_id1 = "e49eab1313ec3171b5c7a483aac9e38d"
+    */
+
+     var options = {
+      host: 'apius.faceplusplus.com',
+      path: '/v2/recognition/compare?api_key='+keys.api_key+'&api_secret='+keys.api_secret+'&face_id2='+face_id2+'&face_id1='+face_id1
+    };
+
+
+
+
+    callback = function(response) {
+            var str = '';
+
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function(chunk) {
+                str += chunk;
+            });
+
+            //the whole response has been recieved, so we just print it out here
+            response.on('end', function() {
+
+                console.log(str);
+            });
+        }    
+
+        http.request(options, callback).end();
+
 }
