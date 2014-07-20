187224e6cc3864edc0e024dad7a298b5d183ffec
Daniel Silver <das2c3@mail.missouri.edu>
2014-07-20 01:59:29 -0500

    almost there on anim

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,19 +1,21 @@
 var faces = [];
+var chart, data, options;
 
 function getMales() {
   var numberOfMales = 0;
   for (i = 0; i < faces.length; i++) {
-    if (faces[i].attribute.gender.value === 'male') {
+    if (faces[i].attribute.gender.value === 'Male') {
       numberOfMales++;
     }
   }
   return numberOfMales;
 }
 
-function getFemales() {
+function getFemales(minAge, maxAge) {
   var numberOfFemales = 0;
   for (i = 0; i < faces.length; i++) {
-    if (faces[i].attribute.gender.value === 'female') {
+    //check for ages, if defined
+    if (faces[i].attribute.gender.value === 'Female') {
       numberOfFemales++;
     }
   }
@@ -22,33 +24,50 @@ function getFemales() {
 
 function drawGenderChart(malePercentage, femalePercentage) {
   // Default to 0 if undefined
-  malePercentage = malePercentage || 0;
-  femalePercentage = femalePercentage || 0;
+  // malePercentage = malePercentage || 0;
+  // femalePercentage = femalePercentage || 0;
 
-  var data = google.visualization.arrayToDataTable([
+  data = google.visualization.arrayToDataTable([
     ['Gender', 'Percentage'],
     ['Male', malePercentage],
     ['Female', femalePercentage]
   ]);
 
-  var options = {
+  options = {
     title: 'Gender Breakdown',
     pieHole: 0.4,
+    animation:{
+      duration: 1000,
+      easing: 'out',
+    }
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('genderChart'));
+  chart = new google.visualization.PieChart(document.getElementById('genderChart'));
+  google.visualization.events.addListener(chart, 'select', function() {
+    var selection = chart.getSelection();
+  });
   chart.draw(data, options);
+  console.log("drawing chart, first time")
 }
 
 function getAnalytics() {
+  console.log("getAnalytics")
   var numberOfMales = getMales(faces);
   var numberOfFemales = getFemales(faces);
   var malePercentage = numberOfMales / faces.length;
   var femalePercentage = numberOfFemales / faces.length;
-  drawGenderChart(malePercentage, femalePercentage);
+  updateGenderChart(malePercentage, femalePercentage);
+}
+
+function updateGenderChart(malePercentage, femalePercentage) {
+  if (data == null) return;
+  data.setValue(0,1,malePercentage)
+  data.setValue(1,1,femalePercentage)
+  chart.draw(data, options)
+  console.log("updating chart")
 }
 
 google.load("visualization", "1", {packages:["corechart"]});
 $(function() {
-  google.setOnLoadCallback(getAnalytics);
+  google.setOnLoadCallback(drawGenderChart(15,10));
 });
