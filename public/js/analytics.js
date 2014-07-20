a410d698aed442304765227ed6528752fabbcc31
JoeLoser <joeloser93@gmail.com>
2014-07-20 01:06:45 -0500

    Working on charts with real data.

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,8 +1,40 @@
-function drawRaceChart() {
+var faces = [];
+/**
+ * @param  array of faces
+ * @return number of males
+ */
+function getMales() {
+  var numberOfMales = 0;
+  for (i = 0; i < faces.length; i++) {
+    if (faces[i].attribute.gender.value === 'male') {
+      numberOfMales++;
+    }
+  }
+  return numberOfMales;
+}
+
+/**
+ * @param  array of faces
+ * @return number of males
+ */
+function getFemales() {
+  var numberOfFemales = 0;
+  for (i = 0; i < faces.length; i++) {
+    if (faces[i].attribute.gender.value === 'female') {
+      numberOfFemales++;
+    }
+  }
+  return numberOfFemales;
+}
+
+function drawGenderChart(malePercentage, femalePercentage) {
+  malePercentage = malePercentage || 0;
+  femalePercentage = femalePercentage || 0;
+
   var data = google.visualization.arrayToDataTable([
     ['Gender', 'Percentage'],
-    ['Male', 60],
-    ['Female', 40]
+    ['Male', malePercentage],
+    ['Female', femalePercentage]
   ]);
 
   var options = {
@@ -10,12 +42,19 @@ function drawRaceChart() {
     pieHole: 0.4,
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  var chart = new google.visualization.PieChart(document.getElementById('genderChart'));
   chart.draw(data, options);
 }
 
-google.load("visualization", "1", {packages:["corechart"]});
+function getAnalytics() {
+  var numberOfMales = getMales(faces);
+  var numberOfFemales = getFemales(faces);
+  var malePercentage = numberOfMales / faces.length;
+  var femalePercentage = numberOfFemales / faces.length;
+  drawGenderChart(malePercentage, femalePercentage);
+}
 
+google.load("visualization", "1", {packages:["corechart"]});
 $(function() {
-  google.setOnLoadCallback(drawRaceChart);
+  google.setOnLoadCallback(drawGenderChart);
 });
--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,15 +1,15 @@
 jQuery(function($){
 	var socket = io.connect();
 
-	var faces = [];
-
 	socket.on('init faces', function(data){
 		console.log(data);
 		faces = data;
+		getAnalytics();
 	});
 
 	socket.on('add faces',function(newFaces){
 		faces = faces.concat(newFaces);
 		console.log(faces);
+		getAnalytics();
 	})
 });
